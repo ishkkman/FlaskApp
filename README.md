e79f44f525e13ee7f0aca34860df042d493a03f6
ishkkman <ishkkman@gmail.com>
2025-01-26 01:28:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FlaskApp
+A simple Flask web application using SQLite as the database.
---
+++ models.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    print("Database created!")
